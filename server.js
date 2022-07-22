12318d7b0193f724635fe321128848ea86c9e7d0
Sean Oh <seanoh@ucsb.edu>
2022-07-22 09:56:29 -0700

    ad pseudocode for server.js

---
+++ server.js
@@ -0,0 +1,26 @@
+// DEPENDENCIES
+
+
+
+// Initiate express
+
+
+// Middleware
+
+
+// Notes GET route
+
+
+// Homepage GET route
+
+
+// Notes API GET route
+
+
+// Notes API POST route
+
+
+// Notes API DELETE route
+
+
+// Port listener
